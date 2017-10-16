08164979811c21fb0c84d753c29014bba22b767d
50616032 <50616032@50616032-pc>
2017-10-16 17:15:27 +0900

    First Commit

---
+++ src/test002/Test002.java
@@ -0,0 +1,19 @@
+package test002;
+
+import java.util.Scanner;
+
+public class Test002 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Input Number> ");
+		int n = Integer.parseInt(scan.next());
+		System.out.println("Number = "+n);
+		
+		System.out.println("Input RealNumber");
+		double m = Double.parseDouble(scan.next());
+		System.out.println("RealNumber = "+m);
+	}
+
+}
